ce014f664dfb06914e72a10a50fb0769adb30884
Mark GR. Miller <mark.gr.miller@bto.org>
2018-03-26 17:42:43 +0100

    starting visualisation

--- rainfall_compile.R
+++ rainfall_compile.R
@@ -32,3 +32,38 @@ plot(raster(ras, layer=5))
 library(maps)
 map('world', add=T)
 
+##### check data
+
+names(ras)
+
+print(raster(ras, layer=1))
+# name dat is 'units: days since 2000-03-01' I took up until June 2017
+names(ras)<-paste(c("03", "04", "05", "06", "07", "08", "09", "10", "11", "12", 
+  rep(c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"), 16),
+  "01", "02", "03", "04", "05", "06"),
+
+c(rep("2000", 10), rep("2001", 12), rep("2002", 12),
+  rep("2003", 12), rep("2004", 12),rep("2005", 12),
+  rep("2006", 12),rep("2007", 12),rep("2008", 12), 
+  rep("2009", 12),rep("2010", 12),rep("2011", 12),
+  rep("2012", 12),rep("2013", 12),rep("2014", 12),
+  rep("2015", 12),rep("2016", 12),rep("2017", 6)), sep="_")
+
+plot(subset(ras, grep("X03",names(ras))))
+
+# ok looks fine
+
+
+# some plotting using tmap
+library(tmap)
+
+tmap_mode('plot')
+
+d4<-read.csv("N:/cuckoo_tracking/data/stopover_1daymin_spring_mig.csv", h=T)
+
+
+
+
+tm_shape(subset(ras,1), projection=projection(ras), 
+         is.master=T, bbox=extent(-17, 33, -17, 26))+tm_raster()+
+          tm_shape(World)+tm_borders()
