c6073aeaf61dccc45e05884304d61cc18936e267
Mark GR. Miller <mark.gr.miller@bto.org>
2018-05-29 13:13:48 +0100

    Change to 'MODIStsp' package (very little code due to GUI use)

--- ndvi_compile.R
+++ ndvi_compile.R
@@ -1,17 +1,16 @@
 ## 25/05/18
 ## Downloading, processing and extracting MODIS NDVI data
 
-library(MODIS)
+# Following tutorial:
+#https://cran.r-project.org/web/packages/MODIStsp/vignettes/MODIStsp.pdf
 
-#check for GDAL
-MODISoptions()
+#install.packages("gWidgetsRGtk2")
+library(gWidgetsRGtk2)
 
-# add GDAL path to list
-Sys.setenv(PATH="C:\\Users\\mark.gr.miller\\Documents\\R\\R-3.4.4\\bin\\x64;C:\\ProgramData\\Oracle\\Java\\javapath;C:\\WINDOWS\\system32;C:\\WINDOWS;C:\\WINDOWS\\System32\\Wbem;C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\;C:\\Users\\mark.gr.miller\\AppData\\Local\\Microsoft\\WindowsApps;C:\\Users\\mark.gr.miller\\AppData\\Local\\Programs\\Git\\cmd;C://Program Files//QGIS 2.18//bin")
+library(devtools)
+install_github("lbusett/MODIStsp")
 
-# recheck
-MODISoptions()
+library(MODIStsp)
+MODIStsp()
+# Thats all you need to use this tool! nice!
 
-ng<-getTile(x='Nigeria')
-
-gd<-runGdal(product='MYD13A2', extent='Nigeria',  begin="2018001", end="2018005")
