14ca0a3bb11f7157f53b0470bc0196f9d8fe3f82
Mark GR. Miller <mark.gr.miller@bto.org>
2018-06-04 16:18:52 +0100

    Switched over to using GRIMMS data

--- ndvi_compile.R
+++ ndvi_compile.R
@@ -65,3 +65,59 @@ for(i in tifz)
   print(i)  
 }
 # much faster!
+
+
+##### Different method !
+### Using GRIMMS cleaned MODIS NDVI product here: https://glam1.gsfc.nasa.gov/
+### Make Spatial grid that matches extractable one on website (LIS 0.25 degree)
+### Index grid to match website then assign each stopover to a 0.25 cell and 
+### pull these from the website. This provides a nifty csv with NDVI and anomaly
+### data in the cell from 2003 until present.
+library(raster)
+library(sp)
+library(rgdal)
+
+# LIS grid is full longitude but only -60 to 80 latitude
+# these are index values 
+exp1<-(expand.grid(1:1440, 560:1))
+v2<-as.numeric(paste(exp1[,1], exp1[,2], sep='.'))
+
+r<-raster(nrows=(140*4), ncols=(360*4), xmn=-180, xmx=180, ymn=-60, ymx=80, 
+       crs='+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', vals=v2)
+
+writeRaster(r, "C:/cuckoo_tracking/data/spatial/marks_LIS_ras.tif")
+
+# nice to have a grid but huge file for whole world, 
+# so just subset to WA
+
+r2<-crop(r, extent(c(-18, 20, -10, 12)))
+plot(r2)
+
+map('world', add=T)
+r.poly <- rasterToPolygons(r2, dissolve=F )
+
+setwd("C:/cuckoo_tracking/data")
+writeOGR(obj=r.poly, dsn="spatial",
+         layer="marks_LIS_grid", driver="ESRI Shapefile",
+         overwrite_layer = T)
+
+## too big just leave as ras
+#sgdf<-as(r, 'SpatialGridDataFrame')
+
+# OK have had a look in QGIS all is correct, now simply bring in stopover data and
+# extract the LIS cell ID elow each stopover. There are some issues here: the cells are 
+# 0.25 degree so the grid won't always have stopover points in the centre, so which
+# cell should be extract from? Could average from neighbours i.g. 4 nearest cells or
+# just take the one over which the point falls. The important thing to rememebr is stopover
+# points are the median location of the stopover (which can cover up to 50km between points)
+
+
+
+https://glam1.gsfc.nasa.gov/wui/4.1.0/chart.html?sat=MYD&mean=2003-2015&layer=NDVI&crop=NONE&type=LIS&n=1&numIds=1&level=null&initYr=2018&id0=0718_269
+
+
+download.file(url='https://glam1.gsfc.nasa.gov/_/csv_2.1?src=https%3A%2F%2Fglam1.gsfc.nasa.gov%2Ftbl%2Fv4%2FMYD%2FGMYD09Q1.6v1.NDVI.MOD44W_2009_land.LIS%2Ftxt%2FGMYD09Q1.6v1.NDVI.MOD44W_2009_land.LIS.0718_269.tbl.txt',
+              destfile = 'C:/cuckoo_tracking/sourced_data/GRIMMS_NDVI/test.csv')
+
+
+
