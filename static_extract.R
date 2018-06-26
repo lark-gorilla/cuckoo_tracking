67de2cbf2ba677d8dfe380d97bd235a220f6d92f
Mark GR. Miller <mark.gr.miller@bto.org>
2018-06-26 13:38:29 +0100

    Extraction of 20m landcover data and distance to steam layer

---
+++ static_extract.R
@@ -0,0 +1,74 @@
+## remote sensing data Rainfall and NDVI extract
+## 30/05/2018
+
+#### work around for different laptop/desktop directories ####
+#### !!!! RUN THIS !!!! ####
+if(Sys.info()['nodename']=="D9L5812"){
+  setwd("C:/cuckoo_tracking")}else{
+    setwd("N:/cuckoo_tracking")}
+#### !!!!!!!!!!!!!!!!!! ####
+
+
+# Use detailed coord data now for extract
+
+dat<-read.csv('data/stopover_bestofday_2018_1daymin_recalc_spring_mig_detailcoords.csv', h=T)
+
+
+dat<-dat[which( ! dat$country%in% c('Algeria', 'France', 'Italy', 'Morocco' ,'Spain',
+                                    'United Kingdom', 'Portugal')),]
+
+
+# data accessed via the portal: http://maps.elie.ucl.ac.be/CCI/viewer/index.php
+
+
+
+library(raster) 
+
+ras<-raster("sourced_data/ESA_CCI_landcover/ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0/ESACCI-LC-L4-LC10-Map-20m-P1Y-2016-v1.0.tif")
+
+#landcover<-extract(ras, dat[, 23:24], buffer=150, df=T) # add 1500 m buffer as landcover is c. 250 m resolution
+## only use 150m cos 1500 grabs too many!
+
+#landcover2<-extract(ras, dat[, 23:24], buffer=1500, fun=function(x){table(x)}) # add 1500 m buffer as landcover is c. 250 m resolution
+## only use 150m cos 1500 grabs too many!
+
+landcover3<-extract(ras, dat[, 23:24], buffer=1500) # add 1500 m buffer as landcover is c. 20 m!! resolution
+## on
+lc3<-lapply(landcover3, FUN=function(x)
+{
+  data.frame(matrix(table(x), ncol=length(table(x)),
+                    dimnames=list(NULL,unlist(dimnames(table(x))))))
+})
+
+library(dplyr)
+
+lc3a<-bind_rows(lc3) # binds up rows if columns don't match gives an NA - sweet!
+
+dat<-cbind(dat, lc3a)
+
+names(dat)[names(dat)=='X1']<-"Trees"
+names(dat)[names(dat)=='X2']<-"Shrubs"
+names(dat)[names(dat)=='X3']<-"Grasslands"
+names(dat)[names(dat)=='X4']<-"Croplands"
+names(dat)[names(dat)=='X5']<-"Aquatic veg reg flooded"
+names(dat)[names(dat)=='X7']<-"Bare areas"
+names(dat)[names(dat)=='X8']<-"Built up areas"
+names(dat)[names(dat)=='X10']<-"Open water"
+names(dat)[names(dat)=='X200']<-"NA"
+
+
+write.csv(dat, "data/stopover_bestofday_2018_1daymin_recalc_spring_mig_detailcoords_landcov_ext.csv", quote=F, row.names=F)
+
+## read in rivers layer
+
+library(rgdal)
+
+setwd("C:/cuckoo_tracking/sourced_data/rivers")
+rivers<-readOGR(layer="rivers_africa_37333",
+                dsn="rivers_africa_37333") # different linux/windows
+
+projection(rivers)
+
+library(geosphere)
+
+dat$riverDist<-dist2Line(dat[, 23:24], rivers)
