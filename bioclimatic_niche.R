4d9af9923e992d2d80e109bfb2da004a4b02b353
Mark GR. Miller <mark.gr.miller@bto.org>
2018-07-04 17:41:47 +0100

    progress on rainfall and ndvi

--- bioclimatic_niche.R
+++ bioclimatic_niche.R
@@ -35,16 +35,95 @@ dat2<- dat2 %>% select(year, ptt, SO_startDOY, SO_endDOY, country, everything())
 precip<-dat2[,1:8]
 
 # add cumulative rainfall
-precip[which(is.na(precip$precip)),]$precip # fix na
+precip[which(precip$precip>500),]$precip<-0 # remove erroneous vals
+precip[which(is.na(precip$precip)),]$precip<-0 # fix na
 
 precip<-precip %>% group_by(year, ptt, SO_startDOY) %>% dplyr::mutate(cumrf=cumsum(precip))
 
+# Not doing now as drops needed info
 # drops duplicated daily values between 8 day modis images
-ndvi<-dat2[dat2$variable %in% seq(1, 125, 8),] 
+#ndvi<-dat2[dat2$variable %in% seq(1, 125, 8),]
+
+ndvi<-dat2
+ndvi$precip<-NULL
+
+# add month and region
+ndvi$month=1
+ndvi[ndvi$variable %in% (32:59),]$month=2
+ndvi[ndvi$variable %in% (60:90),]$month=3
+ndvi[ndvi$variable %in% (91:125),]$month=4
+
+ndvi$region<-'WestAF'
+
+ndvi[ndvi$country %in% c("Congo","Cameroon",
+                     "Central African Republic",
+                     "Democratic Republic of the Congo",
+                     "Angola", "Gabon"),]$region<-"CentralAF"
+
+
+# ndvi plots
+
+#across all sites
+p<-ggplot(data=ndvi, aes(x=NDVI, fill=cuck_pres))
+
+p+geom_histogram(position='dodge')+facet_wrap(~month)
+
+p+geom_histogram(position='dodge')+facet_grid(region~month, scales='free_y')
+
+# order countries by longitude
+levels(ndvi$country)
+ndvi$country = factor(ndvi$country,
+                      levels(ndvi$country)[c(1,2,11,13,10,6,9,14,12,3,4,8,5,7)])
+
+p<-ggplot(data=ndvi[-which(ndvi$country %in% c('Angola', 'Burkina Faso', 'Liberia')),],
+          aes(x=aquaNDVI, fill=cuck_pres))
+
+jpeg("outputs/aquaNDVI_month_country.jpg",
+     width =   11.69, height =8.27 , units ="in", res =300)
+
+p+geom_histogram(position='dodge', binwidth=0.05)+
+  scale_fill_manual(values=c("N" = "grey", "Y"="black"))+
+   facet_grid(month~country, scales='free_y')+
+  theme_bw()+theme(legend.position='none')
+
+dev.off()
+
+jpeg("outputs/aquaNDVI_year_country.jpg",
+     width =   11.69, height =8.27 , units ="in", res =300)
+
+p+geom_histogram(position='dodge', binwidth=0.05)+
+  scale_fill_manual(values=c("N" = "grey", "Y"="black"))+
+  facet_grid(year~country, scales='free_y')+
+  theme_bw()+theme(legend.position='none')
+
+dev.off()
 
 # rainfall plots
 
-p<-ggplot(data=precip[precip$variable %in% seq(1:31),],
-          aes(x=cumrf, colour=cuck_pres))
+precip$month=1
+precip[precip$variable %in% (32:59),]$month=2
+precip[precip$variable %in% (60:90),]$month=3
+precip[precip$variable %in% (91:125),]$month=4
 
-p+geom_histogram(position='dodge')
+# ndvi plots
+
+#across all sites
+
+# order countries by longitude
+levels(precip$country)
+
+precip$country = factor(precip$country,
+                      levels(precip$country)[c(13,11,10,6,9,14,2,12,3,4,8,5,7,1)])
+
+p<-ggplot(data=precip[precip$cumrf<1000,], aes(y=..density..,x=cumrf, fill=cuck_pres))
+
+p+geom_histogram(position='dodge', binwidth=20)+
+  scale_fill_manual(values=c("N" = "grey", "Y"="black"))+
+  facet_grid(country~., scales='free_y')+
+  theme_bw()+theme(legend.position='none')
+
+p<-ggplot(data=precip[precip$cumrf>0,], aes(x=cumrf, fill=cuck_pres))
+
+ p+geom_histogram(position='dodge', binwidth=10)+
+   scale_fill_manual(values=c("N" = "grey", "Y"="black"))+
+  theme_bw()+theme(legend.position='none')
