b979d6d154cbd50c36920f0ed3f6d7363a5bc74b
lark-gorilla <mark.gr.miller@gmail.com>
2018-10-22 11:38:04 +0100

    Edits to add migration stages in Autumn

--- paper_analyses.R
+++ paper_analyses.R
@@ -11,9 +11,9 @@ library(sf)
 library(dplyr)
 
 
-dmod<-read.csv('C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready.csv', h=T)
+dmod<-read.csv('~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready.csv', h=T)
 
-bodpoints<-read.csv('C:/cuckoo_tracking/data/processed_movebank_cuckoos_hybrid_filter_bestofday_2018_clean_stopovers_recalc.csv', h=T)
+bodpoints<-read.csv('~/BTO/cuckoo_tracking/data/processed_movebank_cuckoos_hybrid_filter_bestofday_2018_clean_stopovers_recalc.csv', h=T)
 
 bodpoints$pttyear<-paste(bodpoints$ptt, bodpoints$year.x)
 dmod$pttyear<-paste(dmod$ptt, dmod$year)
@@ -23,7 +23,7 @@ dmod$pttyear<-paste(dmod$ptt, dmod$year)
 bodpoints<-bodpoints %>% filter(pttyear %in% unique(dmod$pttyear) & julian<145)
 
 
-wrld<-st_read('C:/cuckoo_tracking/sourced_data/country_borders/TM_WORLD_BORDERS-0.3.shp')
+wrld<-st_read('~/BTO/cuckoo_tracking/sourced_data/country_borders/TM_WORLD_BORDERS-0.3.shp')
 
 bodpoints_sp0<-st_as_sf(bodpoints, coords = c("long","lat"),
                  crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
@@ -94,12 +94,12 @@ dmod<-left_join(dmod, dp1[,1:4], by=c('ptt', 'year'='year.x'))
 
 ## Now correct departure from wintering ground
 
-d2<-read.csv("C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ALL_mig.csv", h=T)
+d2<-read.csv("~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ALL_mig.csv", h=T)
 
 
-dat2<-read.csv("C:/cuckoo_tracking/data/stopover_table_bestofday_2018_1daymin_recalc_biomes.csv", h=T)
+dat2<-read.csv("~/BTO/cuckoo_tracking/data/stopover_table_bestofday_2018_1daymin_recalc_biomes.csv", h=T)
 
-UK_travel<-read.csv("C:/cuckoo_tracking/data/complete_cycles_UK_all_timing.csv", h=T)
+UK_travel<-read.csv("~/BTO/cuckoo_tracking/data/complete_cycles_UK_all_timing.csv", h=T)
 
 # manually add 4 columns for 2018 spring migration birds. As only best of day 
 # data downloaded for 2018 entry datestime is estimated from movebank
@@ -208,7 +208,7 @@ dater$SO_endDOY<-yday(
   dater$SO_end) 
 
 #write out
-#write.csv(dater, "C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ALL_mig_liveNdead.csv", quote=F, row.names=F)
+#write.csv(dater, "~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ALL_mig_liveNdead.csv", quote=F, row.names=F)
 
 
 # group by mig-cohort rather than year but 
@@ -276,7 +276,7 @@ dmod<-dmod[,c(1:5, 24, 6:11, 23)]
 
 # add mig route back in
 
-temp<-read.csv('C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready.csv', h=T)
+temp<-read.csv('~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready.csv', h=T)
 
 # fix to remove duplicate Livingstone in new dataset
 
@@ -291,13 +291,122 @@ dmod$DEPeurope<-NULL
 
 #write out
 
-write.csv(dmod, "C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_final.csv", quote=F, row.names=F)
+write.csv(dmod, "~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_final.csv", quote=F, row.names=F)
 
 # use previous modelready dataset to attrib env data to new dmod
 
 dmodA<-cbind(dmod, temp[,c(22:24,31:126)])
 
-write.csv(dmodA, "C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready_final.csv", quote=F, row.names=F)
+write.csv(dmodA, "~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready_final.csv", quote=F, row.names=F)
+
+### New request (19/10/18): add migration milestones back through Autumn migration
+### last day on breeding grounds
+### last stopover prior to Sahara crossing
+### Departure from the Sahel
+
+dmod<-read.csv('~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_final.csv', h=T)
+
+bodpoints<-read.csv('~/BTO/cuckoo_tracking/data/processed_movebank_cuckoos_hybrid_filter_bestofday_2018_clean_stopovers_recalc.csv', h=T)
+
+bodpoints$pttyear<-paste(bodpoints$ptt, bodpoints$year.x)
+dmod$pttyear<-paste(dmod$ptt, (dmod$year-1)) # drop year by 1 to capture autumn mig
+
+#filter to just birds in dmod (year-1) and autumn migration
+
+bodpoints<-bodpoints %>% filter(pttyear %in% unique(dmod$pttyear) & julian>145)
+
+
+wrld<-st_read('~/BTO/cuckoo_tracking/sourced_data/country_borders/TM_WORLD_BORDERS-0.3.shp')
+
+bodpoints_sp0<-st_as_sf(bodpoints, coords = c("long","lat"),
+                        crs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
+
+bodpoints_sp0$lat=bodpoints$lat
+bodpoints_sp0$long=bodpoints$long
+bodpoints_sp0$mtype=bodpoints$mtype
+
+bodpoints_sp0<-select(bodpoints_sp0, ptt, year.x, julian, lat, long, mtype)
+
+wrld$NAME=as.character(wrld$NAME)
+
+bodpoints_sp<-st_join(bodpoints_sp0, wrld[c('NAME', 'REGION', 'SUBREGION')])
+
+pts<-st_intersects(bodpoints_sp0, wrld)
+
+pts_logical = lengths(pts) == 0
+
+sea_pts<-bodpoints_sp0[pts_logical,]
+
+sea_pts$NAME='SEA'
+sea_pts$REGION=999
+sea_pts$SUBREGION=999
+
+bodpoints_sp<-rbind(bodpoints_sp, sea_pts)
+
+# ok looks good
+plot(bodpoints_sp['REGION'])
+
+bodpoints_sp_ext<-bodpoints_sp
+
+bodpoints_sp_ext$geometry<-NULL
+
+## subset and join for North Africa
+
+dp1<-bodpoints_sp_ext %>% as_tibble() %>% filter(NAME=='United Kingdom') %>%
+  group_by(ptt, year.x) %>% summarise_all(last)
+
+dp1<-rename(dp1, DEPuk=julian)
+
+#drop env data
+dmod$year_1<-dmod$year-1
+
+dmod<-left_join(dmod, dp1[,1:3], by=c('ptt', 'year_1'='year.x'))
+
+## subset and join for depart Europe/Morocco, basically last stopover before sahara crossing
+
+dp1<-bodpoints_sp_ext %>% as_tibble() %>%
+  filter(NAME %in% c("United Kingdom","Belgium","Germany",
+                     "Italy","France", "Spain",
+                     "Netherlands","Luxembourg", "Morocco", "Czech Republic", 
+                     "Austria", "Slovakia" , "Montenegro" ,   "Albania"  , 
+                     "Bosnia and Herzegovina", "Greece", "Croatia", "Switzerland", 
+                     "Hungary", "Poland") & mtype == 'S') %>%
+  group_by(ptt, year.x) %>%
+  summarise_all(last)
+
+dp1<-rename(dp1, ARRsahara=julian, ARRsaharacountry=NAME)
+
+dmod<-left_join(dmod, dp1[,1:3], by=c('ptt', 'year_1'='year.x'))
+
+
+## Now for departure from the Sahel
+
+dp1<-bodpoints_sp_ext %>% as_tibble() %>%
+  filter(lat>9 & mtype == 'S' & NAME!='SEA') %>%
+  group_by(ptt, year.x) %>%
+  summarise_all(last)
+
+dp1<-rename(dp1, DEPsahel=julian, DEPsahelcountry=NAME)
+
+dmod<-left_join(dmod, dp1[,c(1:3,7)], by=c('ptt', 'year_1'='year.x'))
+
+dmod1<-select(dmod, ptt, year, dead, breeding_loc, breeding_hab, autumn_mig, DEPuk,
+              ARRsahara, DEPsahel, depart_winterSO, DEPcentralAF, DEPwestAF, DEPnorthAF, 
+              arrive_uk, arrive_breeding)
+
+
+write.csv(dmod1, '~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ANNUAL_mig_summary_dead_final.csv', quote=F, row.names=F)
+
+# now calc durations in each region
+
+dmod2=dmod1
+
+dmod2[,10:15]<-dmod2[,10:15]+365
+
+dmod2[,8:15]<-apply(dmod2[,7:15],1, diff)
+
+
+write.csv(dmod2, '~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_ANNUAL_DIFF_mig_summary_dead_final.csv', quote=F, row.names=F)
 
 
 ##############################
@@ -311,8 +420,7 @@ write.csv(dmodA, "C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc
 library(lmerTest) # internally calls lme4
 library(ggplot2)
 
-dmod<-read.csv("C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_final.csv", h=T)
-
+dmod<-read.csv("~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_final.csv", h=T)
 
 m1<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab/ptt)+(1|year), data=dmod)
 m2<-lmer(arrive_breeding~DEPnorthAF+(1|breeding_hab/ptt)+(1|year), data=dmod)
@@ -345,12 +453,12 @@ ggplot(data=dfresid, aes(x=mig_region, y=residuals))+geom_point(shape=1)+
 
 dmod_nona<-na.omit(dmod)
 
-m1<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
-m2<-lmer(arrive_breeding~DEPnorthAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
-m3<-lmer(arrive_breeding~DEPwestAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
+m1n<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
+m2n<-lmer(arrive_breeding~DEPnorthAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
+m3n<-lmer(arrive_breeding~DEPwestAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
 
-m5<-lmer(arrive_breeding~DEPcentralAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
-m6<-lmer(arrive_breeding~depart_winterSO+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
+m5n<-lmer(arrive_breeding~DEPcentralAF+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
+m6n<-lmer(arrive_breeding~depart_winterSO+(1|breeding_hab/ptt)+(1|year), data=dmod_nona)
 
 #make predictions and plots
 
@@ -547,7 +655,7 @@ qplot(data=var_yr, x=variable, y=value)+facet_wrap(~year)
 ### Run investigation of env impacts on W Africa departure date
 ## as per discussion with James
 
-modenv<-read.csv('C:/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready_final.csv', h=T)
+modenv<-read.csv('~/BTO/cuckoo_tracking/data/stopover_bestofday_2018_1daymin_recalc_spring_mig_summary_dead_attrib_modelready_final.csv', h=T)
 
 
 m1<-lmer(DEPwestAF~DEPcentralAF+monsTarr_mean+
@@ -588,3 +696,88 @@ mm1<-lm(var.Sahara+var.europe~emodisNDVI_mean, data=dmod_nona)
 summary(mm1)
 anova(mm1)
 #hmm
+
+### section for chris 09/10/18
+# checking difference between r2 for normal models and na.omit models
+data.frame(mod=c('Arr UK', 'Arr Europe', 'Dep WA', 'Dep CA', 'Dep winter'),
+           m1=c(r.squaredGLMM(m1)[1],r.squaredGLMM(m2)[1],r.squaredGLMM(m3)[1],r.squaredGLMM(m5)[1],r.squaredGLMM(m6)[1]),
+           m1NA=c(r.squaredGLMM(m1n)[1],r.squaredGLMM(m2n)[1],r.squaredGLMM(m3n)[1],r.squaredGLMM(m5n)[1],r.squaredGLMM(m6n)[1]),
+           c1=c(r.squaredGLMM(m1)[2],r.squaredGLMM(m2)[2],r.squaredGLMM(m3)[2],r.squaredGLMM(m5)[2],r.squaredGLMM(m6)[2]),
+           c1NA=c(r.squaredGLMM(m1n)[2],r.squaredGLMM(m2n)[2],r.squaredGLMM(m3n)[2],r.squaredGLMM(m5n)[2],r.squaredGLMM(m6n)[2]))
+           
+
+# Checking marginal increase in R2 with adding additional stages sequentially
+
+
+m1<-lmer(arrive_breeding~depart_winterSO+DEPcentralAF+DEPwestAF+DEPnorthAF+arrive_uk+(1|breeding_hab/ptt)+(1|year), data=dmod)
+m2<-lmer(arrive_breeding~depart_winterSO+DEPcentralAF+DEPwestAF+DEPnorthAF+(1|breeding_hab/ptt)+(1|year), data=dmod)
+m3<-lmer(arrive_breeding~depart_winterSO+DEPcentralAF+DEPwestAF+(1|breeding_hab/ptt)+(1|year), data=dmod)
+
+m5<-lmer(arrive_breeding~depart_winterSO+DEPcentralAF+(1|breeding_hab/ptt)+(1|year), data=dmod)
+m6<-lmer(arrive_breeding~depart_winterSO+(1|breeding_hab/ptt)+(1|year), data=dmod)
+
+r.squaredGLMM(m6);r.squaredGLMM(m5);r.squaredGLMM(m3);r.squaredGLMM(m2);r.squaredGLMM(m1)
+
+# Check effect of different RE specification
+
+r.squaredGLMM(lmer(arrive_breeding~depart_winterSO+(1|breeding_hab/ptt)+(1|year), data=dmod))
+r.squaredGLMM(lmer(arrive_breeding~depart_winterSO+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod))
+#same
+r.squaredGLMM(lmer(arrive_breeding~depart_winterSO+(1|breeding_hab/ptt)+(depart_winterSO|year), data=dmod))
+r.squaredGLMM(lmer(arrive_breeding~depart_winterSO+(depart_winterSO|breeding_hab)+(1|ptt)+(depart_winterSO|year), data=dmod))
+# var explained by RE shoots up
+
+## Have a look what general picture is
+
+m1<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+m2<-lmer(arrive_breeding~DEPnorthAF+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+m3<-lmer(arrive_breeding~DEPwestAF+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+m5<-lmer(arrive_breeding~DEPcentralAF+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+m6<-lmer(arrive_breeding~depart_winterSO+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+
+m1a<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab)+(1|ptt)+(arrive_uk|year), data=dmod)
+m2a<-lmer(arrive_breeding~DEPnorthAF+(1|breeding_hab)+(1|ptt)+(DEPnorthAF|year), data=dmod)
+m3a<-lmer(arrive_breeding~DEPwestAF+(1|breeding_hab)+(1|ptt)+(DEPwestAF|year), data=dmod)
+m5a<-lmer(arrive_breeding~DEPcentralAF+(1|breeding_hab)+(1|ptt)+(DEPcentralAF|year), data=dmod)
+m6a<-lmer(arrive_breeding~depart_winterSO+(1|breeding_hab)+(1|ptt)+(depart_winterSO|year), data=dmod)
+
+m1b<-lmer(arrive_breeding~arrive_uk+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod)
+m2b<-lmer(arrive_breeding~DEPnorthAF+(DEPnorthAF|breeding_hab)+(1|ptt)+(DEPnorthAF|year), data=dmod)
+m3b<-lmer(arrive_breeding~DEPwestAF+(DEPwestAF|breeding_hab)+(1|ptt)+(DEPwestAF|year), data=dmod)
+m5b<-lmer(arrive_breeding~DEPcentralAF+(DEPcentralAF|breeding_hab)+(1|ptt)+(DEPcentralAF|year), data=dmod)
+m6b<-lmer(arrive_breeding~depart_winterSO+(depart_winterSO|breeding_hab)+(1|ptt)+(depart_winterSO|year), data=dmod)
+
+
+data.frame(mod=c('Arr UK', 'Arr Europe', 'Dep WA', 'Dep CA', 'Dep winter'),
+           m1=c(r.squaredGLMM(m1)[1],r.squaredGLMM(m2)[1],r.squaredGLMM(m3)[1],r.squaredGLMM(m5)[1],r.squaredGLMM(m6)[1]),
+           c1=c(r.squaredGLMM(m1)[2],r.squaredGLMM(m2)[2],r.squaredGLMM(m3)[2],r.squaredGLMM(m5)[2],r.squaredGLMM(m6)[2]),
+           m2=c(r.squaredGLMM(m1a)[1],r.squaredGLMM(m2a)[1],r.squaredGLMM(m3a)[1],r.squaredGLMM(m5a)[1],r.squaredGLMM(m6a)[1]),
+           c2=c(r.squaredGLMM(m1a)[2],r.squaredGLMM(m2a)[2],r.squaredGLMM(m3a)[2],r.squaredGLMM(m5a)[2],r.squaredGLMM(m6a)[2]),
+           m3=c(r.squaredGLMM(m1b)[1],r.squaredGLMM(m2b)[1],r.squaredGLMM(m3b)[1],r.squaredGLMM(m5b)[1],r.squaredGLMM(m6b)[1]),
+           c3=c(r.squaredGLMM(m1b)[2],r.squaredGLMM(m2b)[2],r.squaredGLMM(m3b)[2],r.squaredGLMM(m5b)[2],r.squaredGLMM(m6b)[2]))
+           
+
+# Compare effect of REs when considered as fixed effects
+
+r.squaredGLMM(lmer(arrive_breeding~depart_winterSO+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod))
+# basically all REs R2c=0.43
+summary(lm(arrive_breeding~depart_winterSO+breeding_hab+factor(ptt)+factor(year), data=dmod))
+# Adjusted R-squared:  0.5158 
+mayte<-lm(arrive_breeding~depart_winterSO+breeding_hab+factor(ptt)+factor(year), data=dmod)
+anova(mayte) # looks driven by breeding hab
+#library(relaimpo)
+#calc.relimp(mayte)
+
+# this should have more var explained by stage
+
+r.squaredGLMM(lmer(arrive_breeding~DEPwestAF+(1|breeding_hab)+(1|ptt)+(1|year), data=dmod))
+# R2c= 0.75
+summary(lm(arrive_breeding~DEPwestAF+breeding_hab+factor(ptt)+factor(year), data=dmod))
+# Adjusted R-squared:  0.65
+mayte<-lm(arrive_breeding~DEPwestAF+breeding_hab+factor(ptt)+factor(year), data=dmod)
+anova(mayte) 
+
+
+
+
+
